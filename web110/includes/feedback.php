740f99d0f6cf509fe7213d8fccad1d1a3bba6685
Daniel Bateman <batemand213@gmail.com>
2020-07-09 05:53:12 -0700

    Adds initial WEB162 files
    Discludes the contact_include file

---
+++ web110/includes/feedback.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Daniel Bateman: WEB110 Portal Website</title>
+  <meta charset="utf-8" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" type="text/css" href="css/portal.css" />
+  <link rel="stylesheet" type="text/css" href="css/nav.css" />
+ 
+ </head>
+ <body>
+     <header>
+     <h1>Daniel Bateman: WEB110 Portal Website</h1>
+     <nav class="topnav" id="myTopnav">
+       <a href="index.html" class="active">Welcome</a>
+       <a href="elements.html">HTML Elements</a>
+       <a href="big/index.html">BIG</a>
+       <a href="aia.html">AIA</a>
+       <a href="flowchart.html">Flowchart</a>
+       <a href="fp/index.html">Final Project</a>
+       <a href="http://w3schools.com" target="_blank">W3 Schools</a>
+       <a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>
+       <a href="contact.php">Contact Daniel Bateman</a>
+       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </nav>
+     </header>
+     
+   <main class="wrapper">
+     
+   
+       
+       <div class="contact-feedback">
+    <h2>Your Comments Have Been Received!</h2>
+    <p>Thanks for the input!</p>
+    <p>I will respond via email within 24 hours, if you requested information.</p>
+</div> 
+       
+       
+       
+       
+       
+       
+    
+       
+    
+      
+
+     
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+        <footer>
+      <p><small>&copy; 2019 by <a href="contact.php">Contact Daniel Bateman </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+  </main>
+     
+  <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
+     
+  <script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }   
+  </script>
+     
+ </body>
+</html>
